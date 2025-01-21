d053f354821cfb918e156b1cbb017f4f6261e7c3
mluadassio <malucardperad@gmail.com>
2025-01-20 23:10:51 -0300

    começando o novo curso treinando referente ao jogo

---
+++ 03-logica-de-programacao-praticando-em-desafios/sorteador-numeros/app.js
@@ -0,0 +1,31 @@
+let contagemInicial = document.getElementById(de);
+let contagemFinal = document.getElementById(ate);
+let quantidadeDeNumero = document.getElementById(quantidade);
+let numeroSorteado;
+
+function contagem(){ 
+    while (contagemInicial <= contagemFinal){
+    
+        console.log(contagemInicial);
+        contagemInicial++;
+       
+    }
+    
+}
+
+
+function sortear(){
+    
+    while (contagemInicial <= contagemFinal){
+    
+        console.log(contagemInicial);
+        contagemInicial++;
+       
+    }
+
+}
+
+
+
+
+
