6698e8b01ea2e24eb0dc8be5982880e58b3f6cab
mluadassio <malucardperad@gmail.com>
2025-02-02 21:10:01 -0300

    finalizando projeto 2

--- 03-logica-de-programacao-praticando-em-desafios/alugames/js/app.js
+++ 03-logica-de-programacao-praticando-em-desafios/alugames/js/app.js
@@ -2,15 +2,23 @@ function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nameGame = gameClicado.querySelector('.dashboard__item__name');
 
-    if(imagem.classList.contains ("dashboard__item__img--rented")){
-        imagem.classList.remove ("dashboard__item__img--rented");
-        botao.classList.remove ("dashboard__item__button--return");
-        botao.textContent = 'Ativo';
-    } else {
+    if(imagem.classList.contains ('dashboard__item__img--rented')){
+        
+
+        if(confirm(`Você quer devolvero o jogo ${nameGame.textContent}?`)){
+            imagem.classList.remove ("dashboard__item__img--rented");
+            botao.classList.remove ("dashboard__item__button--return");
+            botao.textContent = 'Alugar';
+            qntAlugado()
+        } 
+    }else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
     }
+
+    
 }
 
